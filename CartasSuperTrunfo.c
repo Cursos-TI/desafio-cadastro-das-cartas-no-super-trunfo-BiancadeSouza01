052f67ecbfb0ee6ae6fa32f181ad4573330d5120
BiancadeSouza01 <biancafssouza05@gmail.com>
2025-03-07 19:57:14 +0000

    criei comentarios

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,14 +2,35 @@
 
 
 int main() {
+    
     printf("Desafio super trunfo-países!\n");
-    char estado [50]
-    char coidigo da carta []
-    char nome da cidade [] 
-    int população
-    float Area em km²
-    float PIB
-    float numero de pontos turisticos
+    
+     
+    //Carta 1:
+    /*estado: A
+    codigo: A01
+    nome_da_cidade: São Paulo
+    população: 12325000
+    area: 1521.11 km²
+    PIB: 699.28 bilhões de reais
+    Número de Pontos Turísticos: 50
+    */
+
+    //Carta 2:
+    /*Estado: B
+    Código: B02
+    Nome da Cidade: Rio de Janeiro
+    População: 6748000
+    Área: 1200.25 km²
+    PIB: 300.50 bilhões de reais
+    Número de Pontos Turísticos: 30
+    */
+
+    char estado, codigo_da_carta, nome_da_cidade;
+    int populacao, Carta;
+    float area, PIB, numero_de_pontos_turisticos;
+
+    
 
 
 }
